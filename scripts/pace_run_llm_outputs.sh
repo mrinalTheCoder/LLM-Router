b18ce054fadf152c3ef05ee13f0e63da91452e6b
Mrinal Jain <2mrinaljain@gmail.com>
2026-04-19 13:22:46 -0400

    update script

--- scripts/pace_run_llm_outputs.sh
+++ scripts/pace_run_llm_outputs.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem-per-cpu=1G
 #SBATCH -t1:00:00
 #SBATCH --gres=gpu:L40S:1
-#SBATCH -o~/scratch/logs/report-%j.out
+#SBATCH -o/home/hice1/mjain330/scratch/logs/report-%j.out
 
 cd $SLURM_SUBMIT_DIR
 
@@ -11,6 +11,6 @@ module load anaconda3
 module load ollama
 conda activate llm-router
 
-srun run_single_llm_outouts.sh --model "llama3-chatqa:8b"
-srun run_single_llm_outouts.sh --model "llama3.2:3b"
-srun run_single_llm_outouts.sh --model "qwen2.5:3b"
+srun scripts/run_single_llm_outputs.sh --model "llama3-chatqa:8b"
+srun scripts/run_single_llm_outputs.sh --model "llama3.2:3b"
+srun scripts/run_single_llm_outputs.sh --model "qwen2.5:3b"
